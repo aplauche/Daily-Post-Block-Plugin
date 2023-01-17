e3e6f494f93b58e5bc044bee174c16cb715e0706
Anton Plauche <antonplauche92@gmail.com>
2023-01-16 21:06:57 -0600

    clean up js starting point code comment

--- src/blocks/daily-post/index.js
+++ src/blocks/daily-post/index.js
@@ -48,19 +48,3 @@ registerBlockType('fsd/daily-post', {
     );
   },
 });
-
-// registerBlockType('fsd/daily-post', {
-// 	edit() {
-//     const blockProps = useBlockProps();
-
-//     return (
-//       <div {...blockProps}>
-//         <a href="#">
-//           <img src="" />
-//           <h3>Placeholder Title</h3>
-//         </a>
-//       </div>
-//     );
-//   },
-// });
-
